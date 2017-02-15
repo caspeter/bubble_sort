a4d1250bb278b403ba17822e4de2adfcf49e3539
Cassie Peterson <cas.j.peters@gmail.com>
2017-02-15 09:35:40 -0700

    bubble sort function works

---
+++ bubble-sort.js
@@ -0,0 +1,15 @@
+function bubbleSort(array){
+  var done = false;
+  while (done === false){
+    done = true;
+    for(var i=1; i<array.length; i++){
+      if(array[i-1] > array[i]){
+        done = false;
+        [array[i-1], array[i]] = [array[i], array[i-1]];
+      }
+    }
+  }
+  return array
+}
+
+console.log(bubbleSort([4,3,6,8,23,5,7,32]))
